eb201a79652a1837911134929f0d4a16361f9de0
xiayang <2417251701@qq.com>
2024-09-09 09:53:54 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("master test22!");
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
+        System.out.println("push test!");
 
 
     }
